2ea535363bceb0452db9d49f67e5fde9cda3a7f6
Crowdbotics <team@crowdbotics.com>
2021-02-24 06:05:50 +0000

    Update models

--- backend/bonsai_24692/wsgi.py
+++ backend/bonsai_24692/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bonsai_24692.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bonsai_24692.settings")
 
 application = get_wsgi_application()
