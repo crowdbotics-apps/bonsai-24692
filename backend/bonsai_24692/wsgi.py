70f6d2a8cc1c3935bd7677da2f2bcbb6f57b05f2
Crowdbotics <team@crowdbotics.com>
2021-02-24 06:04:25 +0000

    Initial commit.

---
+++ backend/bonsai_24692/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bonsai_24692 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bonsai_24692.settings')
+
+application = get_wsgi_application()
